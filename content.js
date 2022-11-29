9306cdc9681933ca8541edee2db2a0a2b45c50ef
mshahzad257 <114944060+mshahzad257@users.noreply.github.com>
2022-11-29 14:28:13 +0500

    update links properly

--- content.js
+++ content.js
@@ -3,30 +3,28 @@ let dbLinks = [];
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
+    let count = 0;
     fetch('https://tiktok-links.vercel.app/api')
         .then(response => response.json())
         .then(data => {
             data.data.map(link => dbLinks.push(link.url))
             // console.log("dbLinks -> ", dbLinks);
-            console.log(message.txt);
+            // console.log(message.txt);
             if (message.txt === "hello") {
-                const container = document.querySelectorAll(".tiktok-yz6ijl-DivWrapper");
+                const elements = document.querySelectorAll("a[href*='https://www.tiktok.com/@suppli71']");
 
-                container.forEach((item) => {
-                    let link = "";
-                    // check if item has a link
-                    if (item.querySelector("a")) {
-                        // get the link
-                        link = item.querySelector("a").href;
-                    }
+                elements.forEach((item) => {
+                    const secondChild = item.children[1];
+                    const secondChildChild = secondChild.children[0];
+                    const imgElement = secondChildChild.children[0];
 
-                    if (link && dbLinks.includes(link)) {
-                        item.innerHTML = "";
-                        const img = document.createElement("img");
-                        img.src = "https://www.pngmart.com/files/3/Red-Cross-PNG-File.png";
-                        item.appendChild(img);
+                    if (dbLinks.includes(item.href)) {
+                        count++;
+                        imgElement.src = "https://www.pngmart.com/files/3/Red-Cross-PNG-File.png";
                     }
                 });
             }
+            alert("TikTok Links: " + count + " links removed");
         });
 }
+
